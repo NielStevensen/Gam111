eb1464c3ef9d2cda4f05f53e78c75bfa0a3ad448
NielStevensen <40745561+NielStevensen@users.noreply.github.com>
2018-08-22 15:51:32 +1000

    Game Probably finished

---
+++ Assesement3/Gam111Assesment3/Assets/Scripts/Finish.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour {
+
+    public Text Outro;
+    public string SceneToLoad;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        
+        StartCoroutine("Wait");
+        
+    }
+
+    IEnumerator Wait()
+    {
+        Outro.enabled = true;
+        yield return new WaitForSeconds(1);
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
